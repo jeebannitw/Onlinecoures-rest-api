c99db53e234205a0c5d19874b9c219cf423f649f
= <jeebannitw@gmail.com>
2023-02-21 09:51:20 +0530

    added api for showing all courses

---
+++ src/test/java/com/springtest/OnlineCourses/OnlineCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtest.OnlineCourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
